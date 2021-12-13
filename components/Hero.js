73b357b0e1afd7d53d4fc374b05a1c5509ab2d0d
TrueMushReen <steampowered1703@gmail.com>
2021-12-14 00:38:11 +0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -17,7 +17,7 @@ export default function Hero() {
           </RainbowHighlight>
           <RainbowHighlight color={colors[1]}>
             <h1 className="text-4xl md:text-8xl font-bold text-gray-800 dark:text-gray-200 my-2">
-              Livestreamer.
+              Live Operator.
             </h1>
           </RainbowHighlight>
           <RainbowHighlight color={colors[2]}>
