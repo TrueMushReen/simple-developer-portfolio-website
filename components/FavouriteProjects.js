bb131ce76acfebc381e191ee02621a121b89a7c4
TrueMushReen <steampowered1703@gmail.com>
2021-12-14 01:30:11 +0700

    fixing stuffs

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -80,12 +80,12 @@ export default function FavouriteProjects() {
             <div className="relative overflow-hidden shadow-2xl">
               {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
               <img
-                src="/makrab.png"
-                alt="Banner Makrab FASILKOM"
+                src="/Work and Study.png"
+                alt="Poster made for a competition"
                 className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
               />
               <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Makrab FASILKOM
+                Work and Study
               </h1>
               <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 03
