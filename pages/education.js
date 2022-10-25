3245005e20175499b5ae364408bd03c526c4f918
TrueMushReen <steampowered1703@gmail.com>
2022-10-25 11:20:33 +0700

    fixed error 404 when opening education tab

---
+++ pages/education.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Education from "../components/education";
+
+export default function education() {
+  return (
+    <ContainerBlock title="Education - Muhammad Hadi Khoirulloh">
+      <Education />
+    </ContainerBlock>
+  );
+}
