4e281251abb5baf8a9be471eea88705833e3ed9c
TrueMushReen <steampowered1703@gmail.com>
2022-10-25 11:30:46 +0700

    more bugfix

--- components/Education.js
+++ components/Education.js
@@ -1,7 +1,7 @@
 import userData from "@constants/data";
 import React from "react";
 
-export default function Education() {
+export default function Edu() {
   return (
     <section className="bg-white dark:bg-gray-800">
       <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
--- pages/education.js
+++ pages/education.js
@@ -2,7 +2,7 @@ import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
 import Education from "../components/education";
 
-export default function Education() {
+export default function Edu() {
   return (
     <ContainerBlock title="Education - Muhammad Hadi Khoirulloh">
       <Education />
